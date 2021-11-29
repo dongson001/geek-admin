a0dd63268691bd502db7d8c389950b5ca8428ac9
dongson001 <825826837@qq.com>
2021-11-29 11:12:05 +0800

    自定义组件

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -1,13 +1,21 @@
 <template>
   <h1>这是关于页面</h1>
+  <Rate v-model="score"></Rate>
+
+  <Rate v-model="score">课程评分</Rate>
+  <Rate v-model="score">
+    <img width="14" src="/favicon.ico" />
+  </Rate>
+  <!-- <Rate v-model="3.5"></Rate> -->
 </template>
 
-<script>
-export default {
-  setup() {
-    return {};
-  },
-};
+<script setup>
+import { ref } from 'vue';
+import Rate from '../components/Rate.vue';
+let score = ref(3);
+// function update(value) {
+//   score.value = value;
+// }
 </script>
 
 <style lang="scss" scoped></style>
