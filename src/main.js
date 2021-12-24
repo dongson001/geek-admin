5eff7f1d80c7d14c9d84a7f1e6a1e132cf808e6c
dongson001 <825826837@qq.com>
2021-12-24 15:10:48 +0800

    修改错别字

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router/index";
-import sotre from "./sotre/index";
+import store from "./store/index";
 import element3 from 'element3';
 import 'element3/lib/theme-chalk/index.css'
 
-createApp(App).use(element3).use(sotre).use(router).mount("#app");
+createApp(App).use(element3).use(store).use(router).mount("#app");
